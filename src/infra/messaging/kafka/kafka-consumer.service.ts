68cdcbc1d78e6055fd2d0f2564d4cd982396ec02
Thigo <thiago.l17@yahoo.com>
2022-12-22 22:14:31 -0300

    feat(): kafkajs

--- src/infra/messaging/kafka/kafka-consumer.service.ts
+++ src/infra/messaging/kafka/kafka-consumer.service.ts
@@ -2,11 +2,26 @@ import { Injectable, OnModuleDestroy } from '@nestjs/common';
 import { ServerKafka } from '@nestjs/microservices';
 
 @Injectable()
-export class kafkaConsumerService
+export class KafkaConsumerService
   extends ServerKafka
   implements OnModuleDestroy
 {
   constructor() {
-    super({});
+    super({
+      client: {
+        clientId: 'notifications',
+        brokers: ['easy-duckling-12749-us1-kafka.upstash.io:9092'],
+        sasl: {
+          mechanism: 'scram-sha-256',
+          username:
+            'ZWFzeS1kdWNrbGluZy0xMjc0OST1c3g52U3B4b-FMw3t4MsFDgVrm_XSekEwWQc',
+          password: '54f586f706a14aeea24986faf5d1ba31',
+        },
+        ssl: true,
+      },
+    });
+  }
+  async onModuleDestroy() {
+    await this.close();
   }
 }
